eb72ea5c31a7431a56048deae120c0384c876bdf
Elbert <elbert@Elbert.local>
2017-04-15 14:12:08 +0800

    添加TFGirl学习笔记、添加Python3学习笔记

---
+++ ml_cnn/python_data/python3/liaoxuefeng/hello.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+#!/usr/bin/env python
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [body.encode('utf-8')]
+import numpy as np
+
+import pandas
