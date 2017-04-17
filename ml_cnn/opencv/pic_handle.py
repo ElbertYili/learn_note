9a92f4aef5cc9d91dc3e1aec8c34e24248e11793
Elbert <elbert@Elbert.local>
2017-04-17 16:16:52 +0800

    摄像头人脸识别

--- ml_cnn/opencv/pic_handle.py
+++ ml_cnn/opencv/pic_handle.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+face_cascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv3/3.2.0/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+
 def img_sobel(obj):
     #image = cv2.imread(obj)
     gray = cv2.cvtColor(obj, cv2.COLOR_BGR2GRAY)
@@ -12,4 +15,16 @@ def img_sobel(obj):
     gradient = cv2.subtract(gradX, gradY)
     gradient = cv2.convertScaleAbs(gradient)
 
-    return gradient
+    # blurred = cv2.blur(gradient, (9, 9))
+    # (_, thresh) = cv2.threshold(blurred, 90, 255, cv2.THRESH_BINARY)
+    faces = face_cascade.detectMultiScale(gray,
+                                          scaleFactor=1.2,
+                                          minNeighbors=5)
+
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(obj, (x, y), (x + w, y + h), (255, 155, 100), 2)
+
+
+
+    return obj
